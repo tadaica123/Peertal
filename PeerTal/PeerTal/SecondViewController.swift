765ad737f6bf33fa32548d8198ee8eb4318d59b7
tadaica123 <nguyenta.vhp@gmail.com>
2016-10-02 22:42:01 +0700

    Update

---
+++ PeerTal/PeerTal/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PeerTal
+//
+//  Created by Leon Trần on 9/27/16.
+//  Copyright © 2016 PeerTal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
