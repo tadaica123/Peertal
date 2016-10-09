f63eb3efdd214f01e7dd3c37ccaa16de681052a4
tadaica123 <nguyenta.vhp@gmail.com>
2016-10-09 07:21:04 +0700

    Commit lan 2 8/10/2016

--- FindWifiNearly/FindWifiNearly/SearchPageViewController.swift
+++ FindWifiNearly/FindWifiNearly/SearchPageViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class SearchPageViewController: UITableViewController {
+class SearchPageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+      
+        
+        return cell
     }
     
 
